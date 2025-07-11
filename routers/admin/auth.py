a9e76098268b04159ff37b114016bdd317e45660
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-07-11 15:58:01 +0200

    Login form

--- routers/admin/auth.py
+++ routers/admin/auth.py
@@ -1,8 +1,12 @@
-from fastapi import Request
+from fastapi import Request, Form
 from fastapi.responses import HTMLResponse
 from routers.admin.admin import router, templates
 
 
 @router.get('/login', response_class=HTMLResponse)
-async def sing_in(request: Request):
+async def login(request: Request):
     return templates.TemplateResponse('sign-in.html', {"request": request})
+
+@router.post('/login', response_class=HTMLResponse)
+async def authenticate(request: Request, email: str = Form(), password: str= Form()):
+    return templates.TemplateResponse('sign-in.html', {"request": request, 'error_msg': "Invalid credentials."})
