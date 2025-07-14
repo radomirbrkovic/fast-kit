2e82bfd9058b85622456407473a5b85254b46715
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-07-14 22:09:39 +0200

    Add user method in auth service

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import Request
 from passlib.context import CryptContext
 from infastructure.Database import SessionLocal
 from models.user import Users
@@ -8,9 +9,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class AuthService:
     def __init__(self):
         self.db: Session = SessionLocal()
+        self.model = self.db.query(Users)
 
     def authenticate(self, email: str, password: str, role: str):
-        user = self.db.query(Users).filter(Users.email == email).first()
+        user = self.model.filter(Users.email == email).first()
 
         if not user:
             return None
@@ -25,3 +27,13 @@ class AuthService:
 
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
         return pwd_context.verify(plain_password, hashed_password)
+
+    # Get authenticated user by session
+    def user(self, request: Request):
+        if 'auth_id' in request.session:
+            user = self.model.filter(Users.id == request.session['auth_id']).first()
+            if not user:
+                return None
+            return user
+
+        return None
