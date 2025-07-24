7b3a0c743d1d983101e5b6fb1dcdd1cf874cf013
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-07-24 10:28:44 +0200

    Move dashboard behind admin middleware

---
+++ routers/admin/dashboard.py
@@ -0,0 +1,9 @@
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from routers.admin.admin import guard_router, templates
+
+router = guard_router
+
+@router.get('/dashboard', response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse('dashboard.html', {'request': request})
