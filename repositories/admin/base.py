669219f81be23103dd078da69731b28fe51ca44a
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-07-28 18:28:37 +0200

    Base repository

---
+++ repositories/admin/base.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Type, List, Optional
+
+ModelType = TypeVar("ModelType")
+CreateSchemaType = TypeVar("CreateSchemaType")
+UpdateSchemaType = TypeVar("UpdateSchemaType")
+
+class AbstractRepository(ABC, Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+
+    @abstractmethod
+    def get(self) -> List[ModelType]:
+        pass
+
+    @abstractmethod
+    def find(self, id: int) -> Optional[ModelType]:
+        pass
+
+    @abstractmethod
+    def create(self, obj_in: CreateSchemaType) -> ModelType:
+        pass
+
+    @abstractmethod
+    def update(self, id: int, obj_in: UpdateSchemaType) -> Optional[ModelType]:
+        pass
+
+    @abstractmethod
+    def delete(self, id: int) -> None:
+        pass
