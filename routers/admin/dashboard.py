c8a4c62c89aa8aaf2f18e993ef9ccad0b2f7a972
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-07-30 19:31:17 +0200

    Menu

--- routers/admin/dashboard.py
+++ routers/admin/dashboard.py
@@ -4,6 +4,6 @@ from routers.admin.admin import guard_router, templates
 
 router = guard_router
 
-@router.get('/dashboard', response_class=HTMLResponse)
+@router.get('/dashboard', response_class=HTMLResponse, name='admin.dashboard')
 async def dashboard(request: Request):
     return templates.TemplateResponse('dashboard.html', {'request': request})
--- routers/admin/users.py
+++ routers/admin/users.py
@@ -4,6 +4,6 @@ from routers.admin.admin import guard_router, templates
 
 router = guard_router
 
-@router.get('/users', response_class=HTMLResponse)
+@router.get('/users', response_class=HTMLResponse, name="admin.users.index")
 async def index(request: Request):
     return templates.TemplateResponse('users/index.html', {'request': request})
