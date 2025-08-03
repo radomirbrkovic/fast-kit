0ca375e9c27fdf7827e7f3a702e4914c22c6b4d4
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-03 11:07:02 +0200

    User create page layout

--- routers/admin/users.py
+++ routers/admin/users.py
@@ -5,6 +5,7 @@ from routers.admin.admin import guard_router, templates, get_db
 from repositories.admin.user_repository import UserRepository
 from services.admin.user_service import UserService
 from sqlalchemy.orm import Session
+from models.enums import UserRole
 
 router = guard_router
 
@@ -22,4 +23,4 @@ async def index(
 
 @router.get('/users/create', response_class=HTMLResponse, name='admin.users.create')
 async def create(request: Request):
-    return templates.TemplateResponse('users/create.html', {'request': request})
+    return templates.TemplateResponse('users/create.html', {'request': request, 'roles': list(UserRole)})
