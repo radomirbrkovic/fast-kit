0c54321f1f649fac55e6d26173e749aaecf5a180
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-09 18:12:28 +0200

    User token type enums

--- models/enums.py
+++ models/enums.py
@@ -4,3 +4,7 @@ class UserRole(enum.Enum):
     SUPER_ADMIN = "super_admin"
     ADMIN = "admin"
     USER = "user"
+
+class UserTokenType(enum.Enum):
+    RESET_PASSWORD = "reset_password"
+    EMAIL_VERIFICATION = "email_verification"
