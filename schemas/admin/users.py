35155e71e54c55e23cc81ee52587ace4060e8c94
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-09 10:16:44 +0200

    Generate user with generated password

--- schemas/admin/users.py
+++ schemas/admin/users.py
@@ -9,24 +9,10 @@ class UserCreate(BaseModel):
     email: EmailStr
     first_name: str
     last_name: str
-    password: str = Field(min_length=8)
     role: UserRole
     is_active: Optional[bool] = True
     phone_number: Optional[str] = None
     hashed_password: str | None = Field(default=None, exclude=True)
-
-    @field_validator("password")
-    @classmethod
-    def validate_password_strength(cls, v):
-        if not re.search(r"[A-Z]", v):
-            raise ValueError("Password must contain at least one uppercase letter.")
-        if not re.search(r"[a-z]", v):
-            raise ValueError("Password must contain at least one lowercase letter.")
-        if not re.search(r"[0-9]", v):
-            raise ValueError("Password must contain at least one digit.")
-        if not re.search(r"[\W_]", v):
-            raise ValueError("Password must contain at least one special character.")
-        return v
 
     @staticmethod
     def validate_unique_email(v, db: SessionLocal):
--- services/admin/user_service.py
+++ services/admin/user_service.py
@@ -5,6 +5,8 @@ from services.admin.base_crud_service import BaseCrudService, ModelType, CreateS
 from repositories.admin.user_repository import UserRepository
 from models.user import Users
 from passlib.context import CryptContext
+import secrets
+import string
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -17,5 +19,6 @@ class UserService(BaseCrudService[Users, UserCreate, UserUpdate]):
         return [UserOut.model_validate(user) for user in users]
 
     def create(self, obj_in: CreateSchemaType) -> ModelType:
-        obj_in.hashed_password = bcrypt_context.hash(obj_in.password)
+        result = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(8))
+        obj_in.hashed_password = bcrypt_context.hash(result)
         return self.repository.create(obj_in)
