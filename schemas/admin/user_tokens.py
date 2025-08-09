43c2ec8f4db586878dd24f168677fcd00db57397
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-09 18:42:12 +0200

    User token schemas

--- schemas/admin/user_tokens.py
+++ schemas/admin/user_tokens.py
@@ -3,12 +3,4 @@ from models.enums import UserTokenType
 
 class UserTokenCreate(BaseModel):
     user_id: int
-    token: str
     type: UserTokenType
-    expires_at: FutureDate
-
-class UserTokenUpdate(BaseModel):
-    pass
-
-class UserTokenOut(BaseModel):
-    pass
