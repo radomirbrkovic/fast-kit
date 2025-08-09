99cdfe1882e25c3b4535c0b81c2caf8d553ea51d
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-09 09:59:04 +0200

    Send email by gmail smtp

--- utils/email.py
+++ utils/email.py
@@ -1,25 +1,35 @@
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from email.message import EmailMessage
 from dotenv import load_dotenv
+import aiosmtplib
 
 load_dotenv()
 
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
 EMAIL_FROM = os.getenv("EMAIL_FROM")
 
-def send(to_email: str, subject: str, body: str, html: str = None):
-    message = Mail(
-        from_email=EMAIL_FROM,
-        to_emails=to_email,
-        subject=subject,
-        plain_text_content=body,
-        html_content= html
-    )
+async def send(to_email: str, subject: str, body: str, html: str = None):
+    message = EmailMessage()
+    message["From"] = EMAIL_FROM
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.set_content(body)
+
+    if html is not None:
+        message.add_alternative(html)
+
     try:
-        sg = SendGridAPIClient(SENDGRID_API_KEY)
-        response = sg.send(message)
-        return response.status_code
+        await aiosmtplib.send(
+            message,
+            hostname=EMAIL_HOST,
+            port=EMAIL_PORT,
+            username=EMAIL_USER,
+            password=EMAIL_PASS,
+            start_tls=True,
+        )
     except Exception as e:
         print(f"Error: {e}")
         return None
