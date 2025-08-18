f918cd7e860ba604b1019dc639c0970d65946b5a
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-18 22:17:06 +0200

    Pages - model

--- models/page.py
+++ models/page.py
@@ -5,7 +5,7 @@ class Page(Base):
     __tablename__ = "pages"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    slug = Column(String, unique=True, index=True)
+    title = Column(String, nullable=False)
+    slug = Column(String, nullable=False, unique=True, index=True)
     content = Column(Text, nullable=True)
     published_at = Column(DateTime, nullable=True, index=True)
