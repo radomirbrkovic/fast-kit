22dbf7b0cc31511e61d729f4b93f1c908256421b
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-19 22:16:54 +0200

    Page repository

--- repositories/admin/page_repository.py
+++ repositories/admin/page_repository.py
@@ -2,6 +2,6 @@ from repositories.admin.crud_repository import CrudRepository
 from sqlalchemy.orm import Session
 from models.page import Page
 
-class UserRepository(CrudRepository):
+class PageRepository(CrudRepository):
     def __init__(self, db: Session):
         super().__init__(db, Page)
