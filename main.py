ac4f25d64bb65eb4ba74d06a62cf5490bdc9c3b2
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-08-20 11:13:54 +0200

    Add pages in sidebar menu

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from routers.admin.auth import router as admin_auth_router
 from routers.admin.dashboard import router as admin_dashboard_router
 from routers.admin.users import router as admin_users_router
+from routers.admin.pages import router as admin_pages_router
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ app.add_middleware(SessionMiddleware, os.getenv('ADMIN_SECRET_KEY', ''))
 app.include_router(admin_auth_router)
 app.include_router(admin_dashboard_router)
 app.include_router(admin_users_router)
+app.include_router(admin_pages_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/')
---
+++ routers/admin/pages.py
@@ -0,0 +1,17 @@
+from fastapi import Request, Depends, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
+from typing import Optional
+from services.admin.page_service import PageService
+from repositories.admin.page_repository import PageRepository
+from routers.admin.admin import guard_router, get_db, templates
+from sqlalchemy.orm import Session
+
+router = guard_router
+
+def get_service(db: Session = Depends(get_db)):
+    return PageService(PageRepository(db))
+
+@router.get('/pages', response_class=HTMLResponse, name='admin.pages.index')
+async  def index(request: Request, page: Optional[int] = 1, services: PageService = Depends(get_service)):
+    pages = services.get({'page': page})
+    return templates.TemplateResponse('pages/index.html', {'request': request, 'pages': pages})
