5050e0967e4abdec6dd1329869a5faa005976631
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-09-07 15:02:45 +0200

    Add app name in layout

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from routers.admin.auth import router as admin_auth_router
 from routers.admin.dashboard import router as admin_dashboard_router
 from routers.admin.users import router as admin_users_router
 from routers.admin.pages import router as admin_pages_router
+from middlewares.admin_middleware import GlobalContextMiddleware
 
 app = FastAPI()
 
 app.add_middleware(SessionMiddleware, os.getenv('ADMIN_SECRET_KEY', ''))
+app.add_middleware(GlobalContextMiddleware)
 app.include_router(admin_auth_router)
 app.include_router(admin_dashboard_router)
 app.include_router(admin_users_router)
--- middlewares/admin_middleware.py
+++ middlewares/admin_middleware.py
@@ -1,5 +1,17 @@
 from fastapi import Request, HTTPException, status
+import os
+from dotenv import load_dotenv
+from starlette.middleware.base import BaseHTTPMiddleware
 
 def auth(request: Request):
     if request.session.get('auth_id') is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+
+
+class GlobalContextMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        app_name = os.getenv("APP_NAME", "FastKit")
+        request.state.app_name = app_name
+
+        response = await call_next(request)
+        return response
