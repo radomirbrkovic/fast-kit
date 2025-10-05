351ed9cddfd8861e211c53fd62c325c067e1a891
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-05 10:39:50 +0200

    Jwt Handler - verify_token

--- infrastructure/jwt_handler.py
+++ infrastructure/jwt_handler.py
@@ -16,3 +16,10 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
