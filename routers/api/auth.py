471b4ea40e543e168b10193c6e9bf05d08b6f4fe
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-05 12:00:20 +0200

    API Authentication - refresh

--- routers/api/auth.py
+++ routers/api/auth.py
@@ -4,12 +4,22 @@ from services.auth_service import AuthService
 
 auth_service = AuthService()
 
-@router.post("/login", name='api.authentication')
-async def api_login(email: str = Form(...), password: str = Form(...)):
+@router.post("/login", name='api.auth.authentication')
+async def login(email: str = Form(...), password: str = Form(...)):
     result = auth_service.api_authenticate(email, password)
     if not result:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=translation_manager.gettext('api.auth.invalid_credentials')
         )
+    return result
+
+
+@router.post("/refresh",  name='api.auth.refresh')
+def refresh(refresh_token: str = Form(...)):
+    result = auth_service.api_refresh_token(refresh_token)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=translation_manager.gettext('api.auth.invalid_refresh_token'))
     return result
