cd3b1884cb3cb1f5d40253705fa6d0abd592695b
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-05 10:45:24 +0200

    Jwt Handler - create_access_token

--- infrastructure/jwt_handler.py
+++ infrastructure/jwt_handler.py
@@ -7,13 +7,15 @@ import os
 load_dotenv()
 
 SECRET_KEY = os.getenv("API_SECRET_KEY", "fastkit-secretkey")
+REFRESH_SECRET_KEY = os.getenv("API_REFRESH_SECRET_KEY", "astkit-refresh-secretkey")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("API_ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("API_REFRESH_TOKEN_EXPIRE_DAYS", 7)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire, "type": "access"})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
