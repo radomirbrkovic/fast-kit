a990ede647f99746771d86a548d36e47b55d6df5
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-05 11:53:55 +0200

    Api router

--- routers/api/api.py
+++ routers/api/api.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
+from infrastructure.tranaslations import TranslationManager
+
 
 
 public_router = APIRouter(
     prefix='/api',
     tags=['api']
 )
+translation_manager = TranslationManager()
