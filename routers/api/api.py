f765b18412087678f6b38a9d81fff6fece05959e
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-05 12:58:50 +0200

    API Auth Middleware

--- routers/api/api.py
+++ routers/api/api.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 from infrastructure.tranaslations import TranslationManager
+from middlewares.api_auth_middleware import auth
+from fastapi.security import HTTPBearer
 
-
-
+bearer_scheme = HTTPBearer()
 public_router = APIRouter(
     prefix='/api',
     tags=['api']
 )
+
+guarded_router = APIRouter(
+    prefix='/api',
+    tags=['api'],
+    dependencies=[Depends(auth), Depends(bearer_scheme)]
+)
+
 translation_manager = TranslationManager()
