bbfef75fa0c903c6a4d6b449784486a6d98d1bb8
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-10-18 09:58:13 +0200

    DB Agnostic - DatabaseStrategy

---
+++ app/infrastructure/database/strategies/base_strategy.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class DatabaseStrategy(ABC):
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def get_session(self):
+        pass
+
+    @abstractmethod
+    def create_all(self):
+        pass
+
+    @abstractmethod
+    def drop_all(self):
+        pass
